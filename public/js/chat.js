9c8d569f517126e80189904125839217cccc1c5c
Sam Hains <sdh@eml.cc>
2015-08-02 17:21:57 +1000

    messaging fixed

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,11 @@
-var FONT_SIZE = 9;
-var MSG_TIMEOUT = 4000;
-var MSG_Y_SPACING = 16;
-var MSG_REMOVE_TIMER = 8000;
+var appendMessage = function(msg, player) {
+  $('#messages').append($('<li>').text(msg));
+  $('#messages').children(':last-child').css('margin-left', player.position.x);
+  $(".message-list").scrollTop($(".message-list")[0].scrollHeight);
+  $('#messages').children(':last-child').delay(5000).slideUp();
+  $('#messages').append($('<div>'));
+  $('#messages').children(':last-child').delay(5000).slideUp();
+};
 
 var setUpChat = function(socket, room){
     var self = this;
@@ -10,37 +14,22 @@ var setUpChat = function(socket, room){
           
         //$('.minimized-bar').hide();
         $('form').submit(function(e){
-            console.log("form submit!");
 
             var msg = $('#m').val();
             e.preventDefault();
-            $('#messages').append($('<li>').text(msg));
-            console.log('position of player is' , self.player.position.x);
-            $('#messages').children(':last-child').css('margin-left', self.player.position.x);
-            $(".message-list").scrollTop($(".message-list")[0].scrollHeight);
-            $('#messages').children(':last-child').delay(5000).slideUp();
-            socket.emit('chat message', msg);
+            appendMessage(msg, self.player);
             $('#m').val('');
+            socket.emit('chat message', {msg: msg, room: clientRoom, user: clientId});
             return false;
           });
-        //$('.minimize').on('click', function(){
-            //$('.chat-box').hide();
-            ////$('.minimized-bar').show();
-        //});
-        //$('.maximize').on('click', function(){
-            //$('.chat-box').show();
-            //$('.minimized-bar').hide();
-        //});
         socket.on('chat message', function(data){
 
             var remoteId = data.user;
             var msg = data.msg;
             var remotePlayer = remotePlayers[remoteId];
-            $('#messages').append($('<li>').text(msg));
-            $(".message-list").scrollTop($(".message-list")[0].scrollHeight);
+            appendMessage(msg, remotePlayer);
         });              
 
     });
 };
-
 
